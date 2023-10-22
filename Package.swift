65c1c5f10f6ccb81672a1b0b05f11fec69d2d995
Iain McLaren <iain@firesphere.com>
2023-10-22 16:58:48 +1100

    fix: package updates

--- Package.swift
+++ Package.swift
@@ -20,9 +20,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/calmdocs/SwiftWebSocketManager", from: "0.0.13"),
-        .package(url: "https://github.com/calmdocs/SwiftKeyExchange", from: "0.0.7")
-
+        .package(url: "https://github.com/calmdocs/SwiftWebSocketManager", branch: "main"),
+        .package(url: "https://github.com/calmdocs/SwiftKeyExchange", branch: "main"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
